ceb23ec7e72c062752d88c0946525d09515e30b4
Ábel András <119449017+szveg1@users.noreply.github.com>
2025-03-18 22:38:49 +0100

    Modified heightmap generation, so it outputs morton codes instead of float values

--- include/HeightMapGenerator.h
+++ include/HeightMapGenerator.h
@@ -1,18 +1,24 @@
 #pragma once
 #include <vector>
-#include <string>
+#include <random>
 
 class HeightMapGenerator
 {
 public:
-    HeightMapGenerator(unsigned int gridSize);
+    HeightMapGenerator(uint32_t gridSize, int octaves, float persistence, float scale);
     ~HeightMapGenerator();
-
-    std::vector<float> generateHeightMap(int octaves, float persistence, float scale);
-
+    std::vector<uint64_t> generateHeightMapChunk(uint32_t chunkX, uint32_t chunkZ);
 private:
+    uint32_t gridSize;
+    int octaves;
+	float persistence;
+	float scale;
+    float randomOffsetX;
+    float randomOffsetY;
+
+    std::random_device device;
+    std::mt19937 generator;
+    std::uniform_real_distribution<float> distribution;
     float noise(float x, float y);
     float layeredNoise(float x, float y, int octaves, float persistence, float scale);
-
-    unsigned int gridSize;
 };
--- src/HeightMapGenerator.cpp
+++ src/HeightMapGenerator.cpp
@@ -2,51 +2,65 @@
 #include <chrono>
 #include <glm/glm.hpp>
 #include <glm/gtc/noise.hpp>
+#include <morton-nd/mortonND_BMI2.h>
 
-HeightMapGenerator::HeightMapGenerator(unsigned int gridSize) : gridSize(gridSize) {}
+HeightMapGenerator::HeightMapGenerator(uint32_t gridSize, int octaves, float persistence, float scale) 
+    : gridSize(gridSize), octaves(octaves), persistence(persistence), scale(scale)
+{
+    generator = std::mt19937(device());
+    distribution = std::uniform_real_distribution<float>(-1000.0f, 1000.0f);
+    randomOffsetX = distribution(generator);
+    randomOffsetY = distribution(generator);
+}
 
 HeightMapGenerator::~HeightMapGenerator() {}
 
-std::vector<float> HeightMapGenerator::generateHeightMap(int octaves, float persistence, float scale)
+std::vector<uint64_t> HeightMapGenerator::generateHeightMapChunk(uint32_t chunkX, uint32_t chunkZ)
 {
- 
     auto start = std::chrono::steady_clock::now();
 
-    std::vector<float> heightMap(gridSize * gridSize);
+	std::vector<uint64_t> chunk;
 
-    for (unsigned int y = 0; y < gridSize; ++y)
+    for (uint32_t x = chunkX * gridSize; x < (chunkX + 1) * gridSize; x++)
     {
-        for (unsigned int x = 0; x < gridSize; ++x)
+        for (uint32_t z = chunkZ * gridSize; z < (chunkZ + 1) * gridSize; z++)
         {
-            float nx = static_cast<float>(x) / gridSize;
-            float ny = static_cast<float>(y) / gridSize;
-            heightMap[y * gridSize + x] = layeredNoise(nx, ny, octaves, persistence, scale);
+            float nx = static_cast<float>(x) / gridSize + randomOffsetX;
+            float nz = static_cast<float>(z) / gridSize + randomOffsetY;
+            float nheight = layeredNoise(nx, nz, octaves, persistence, scale);
+			uint32_t height = static_cast<uint32_t>(nheight * 255.0f);
+			for (uint32_t y = 0; y < height; y++)
+			{
+				uint64_t morton = mortonnd::MortonNDBmi_3D_64::Encode(x, y, z);
+				chunk.push_back(morton);
+			}
         }
     }
+
 
     auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::duration<float>>(end - start);
 
-    printf("heightmap generation took %.1f seconds\n", duration.count());
+    printf("chunk generation took %.1f seconds\n", duration.count());
 
-    return heightMap;
+    return chunk;
 }
 
-float HeightMapGenerator::noise(float x, float y)
+float HeightMapGenerator::noise(float x, float z)
 {
-    return (glm::simplex(glm::vec2(x, y)) + 1.0f) / 2.0f;
+    return (glm::simplex(glm::vec2(x, z)) + 1.0f) / 2.0f;
 }
 
-float HeightMapGenerator::layeredNoise(float x, float y, int octaves, float persistence, float scale)
+float HeightMapGenerator::layeredNoise(float x, float z, int octaves, float persistence, float scale)
 {
     float total = 0.0f;
     float frequency = scale;
     float amplitude = 1.0f;
     float maxValue = 0.0f; 
 
-    for (int i = 0; i < octaves; ++i)
+    for (int i = 0; i < octaves; i++)
     {
-        total += noise(x * frequency, y * frequency) * amplitude;
+        total += noise(x * frequency, z * frequency) * amplitude;
 
         maxValue += amplitude;
 
